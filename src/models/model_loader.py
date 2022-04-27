15e12618beff0c615651fe9613f7086932ed2e53
ViktorKumpan <skylargreypro@yandex.ru>
2022-04-27 13:53:56 +0000

    Model builder added

--- src/models/model_loader.py
+++ src/models/model_loader.py
@@ -2,12 +2,30 @@
 # coding: utf-8
 
 import torch
-from .ResNetUNet import ResNetUNet
+from .architecture.models import ResNetUNet, AutoEncoder
 
 def load_model(cfg):
     if cfg.name == "ResNetUnet": 
-        model = ResNetUNet(cfg.count_cls, cfg.number_channel)
+        model = ResNetUNet(cfg)
         if hasattr(cfg, "weights_path"):
             print("Load weight in model...")
             model.load_state_dict(torch.load(cfg.weights_path))
+        if hasattr(cfg, "mode") and cfg.mode == "img_to_cls":
+            for name, param in model.named_parameters():
+                if name.find("cls_head") == -1:
+                    param.requires_grad = False
+                else:
+                    param.requires_grad = True   
+    elif cfg.name == "AutoEncoder": 
+        model = AutoEncoder(cfg)
+        if hasattr(cfg, "weights_path"):
+            print("Load weight in model...")
+            model.load_state_dict(torch.load(cfg.weights_path))
+        if hasattr(cfg, "mode") and cfg.mode == "img_to_cls":
+            for name, param in model.named_parameters():
+                if name.find("cls_head") == -1:
+                    param.requires_grad = False
+                else:
+                    print(name)
+                    param.requires_grad = True
     return model
