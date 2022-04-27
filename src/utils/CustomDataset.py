6cdbb0e18c136cb0d5625b9d0d89f66d7c805b74
ViktorKumpan <skylargreypro@yandex.ru>
2022-04-27 13:54:56 +0000

    Added documentation and several functions for working with data

--- src/utils/CustomDataset.py
+++ src/utils/CustomDataset.py
@@ -5,15 +5,17 @@ from torchvision.datasets import CIFAR100
 import numpy as np
 import torch
 from torchvision import transforms
+from typing import Dict
 
 
 class DatasetCIFAR100(Dataset, Args):
     
-    def __init__(self, cfg_dataset):
+    def __init__(self, cfg_dataset:Dict):
         self.args = Args(cfg_dataset)
         
         self.transform = transforms.Compose([
-                transforms.ToTensor()
+                transforms.ToTensor(),
+                transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
                 ])
         self.train_ds, self.val_ds = self.sempler(CIFAR100(self.args.path_to_data,
                                                      transform = self.transform,
@@ -28,8 +30,37 @@ class DatasetCIFAR100(Dataset, Args):
                                 
                                             batch_size = self.args.bs
                                                   )
+        self.visual_ds = None
+        if hasattr(self.args,"visual_val") and self.args.visual_val:
+            self.visual_ds = torch.utils.data.DataLoader(CIFAR100(self.args.path_to_data,
+                                transform = self.transform,
+                                train=False),
+                                            batch_size = 1,
+                                            shuffle=False,
+                                            sampler=SubsetRandomSampler([*range(10)])
+                                            )
+    
+    
+    def post_proc(self,img):
+        """
+        Restoring the image after passing through the model
+        """
+        invTrans = transforms.Compose([ 
+                                lambda x:x.cpu().detach(),
+                                transforms.Normalize(mean = [ 0., 0., 0. ],
+                                                     std = [ 1/0.229, 1/0.224, 1/0.225 ]),
+                                transforms.Normalize(mean = [ -0.485, -0.456, -0.406 ],
+                                                     std = [ 1., 1., 1. ]),
+                                lambda x:x*((2**8) - 1),
+                                lambda x:x.permute((0,3,2,1)),
+                                lambda x:x.type(torch.ByteTensor).numpy()[0]
+                               ])
+        return invTrans(img)
         
     def sempler(self, data_train, batch_size = 4, split = .2):
+        """
+        Splitting a dataset into two with certain proportions
+        """
         data_size = len(data_train)
         
         validation_split = split
@@ -59,3 +90,9 @@ class DatasetCIFAR100(Dataset, Args):
     @property
     def test_loader(self,):
         return self.test_ds
+    @property
+    def visual_loader(self,):
+        return self.visual_ds
+    @property
+    def classes_test(self,):
+        return self.test_ds.dataset.classes
